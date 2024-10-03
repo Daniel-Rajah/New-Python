4caf52a70f8b3afc6aa287fbb96ccb2f23ead1b6
Daniel-Rajah <93043147+Daniel-Rajah@users.noreply.github.com>
2024-10-04 00:04:56 +0100

    First Commit

---
+++ Project 1.py
@@ -0,0 +1,25 @@
+
+"""Introduction to Game"""
+name = input('Hey type your name: ')
+print('Welcome ' + name)
+
+"""Asking user if they want to play"""
+should_we_play = input('Do you want to play an adventure Game? ').lower()
+if should_we_play == 'yes' or 'yh':
+    print('Lets Play!!')
+
+    direction = input('Wanna go left or right?? ').lower()
+    if direction == 'left':
+        print('Guess we going left then...oh wait, theres a tiger...you DEAD!')
+    elif direction == 'right':
+        print('Oh we going right! Lets do it!')
+        
+        question = input('You sure you wanna keep going? ').lower()
+        if question == 'yes' or 'yh':
+            print('you found treasure...Lets goo! You win!')
+        else:
+            print('Lets go back then!')
+else:
+    print('fuck off then!')
+
+
